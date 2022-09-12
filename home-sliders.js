c75271e356264fe96080a18fcc29365f711980fa
Audico-it <112904317+Audico-it@users.noreply.github.com>
2022-09-12 22:11:27 +0300

    Update home-sliders.js

--- home-sliders.js
+++ home-sliders.js
@@ -57,7 +57,7 @@ $(document).ready(function () {
   breakpoints: {
   	      // when window width is >= 320px
     320: {
-      slidesPerView: 3,
+      slidesPerView: 1,
       spaceBetween: 20,
     },
       // when window width is >= 560px
