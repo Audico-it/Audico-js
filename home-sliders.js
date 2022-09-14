9d30c5b7826d0d95352ea6d1dbe803f97be4264a
Audico-it <marcus.paivio@audico.fi>
2022-09-14 21:16:04 +0300

    Update home-sliders.js
    Added freeze to testimonial slider

--- home-sliders.js
+++ home-sliders.js
@@ -27,7 +27,6 @@ $(document).ready(function () {
       spaceBetween: 50
     }
   },
-  //TÄMÄ ON KONMENTTI
       autoplay: {
           delay: 5000,
           disableOnInteraction: false,
@@ -43,57 +42,58 @@ $(document).ready(function () {
       });
     });
 
-/*// Testimonial Slider
+// Testimonial Slider
 $(document).ready(function () {
-      var testimonialslider_wrapper = new Swiper(".testimonialslider_wrapper", {
-          wrapperClass: "testimonialslider_list",
-      slideClass: "testimonialslider_item",
-      navigation: {
-        nextEl: '.testimonialslider-next',
-        prevEl: '.testimonialslider-previous'
-      },
-  slidesPerView: 1,
-  spaceBetween: 10,
-  // Responsive breakpoints
-  breakpoints: {
-  	      // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-      // when window width is >= 560px
-    560: {
-      slidesPerView: 3,
-      spaceBetween: 20
-    },
-    // when window width is >= 820px
-    820: {
-      slidesPerView: 3,
-      spaceBetween: 30
-    },
-    // when window width is >= 1280px
-    1280	: {
-      slidesPerView: 3,
-      spaceBetween: 50
-    }
+  var testimonialslider_wrapper = new Swiper(".testimonialslider_wrapper", {
+      wrapperClass: "testimonialslider_list",
+  slideClass: "testimonialslider_item",
+  navigation: {
+    nextEl: '.testimonialslider-next',
+    prevEl: '.testimonialslider-previous'
   },
-      autoplay: {
-          delay: 100000,
-          disableOnInteraction: false,
+slidesPerView: 1,
+spaceBetween: 10,
+// Responsive breakpoints
+breakpoints: {
+      // when window width is >= 320px
+320: {
+  slidesPerView: 1,
+  spaceBetween: 20,
+},
+  // when window width is >= 560px
+560: {
+  slidesPerView: 3,
+  spaceBetween: 20
+},
+// when window width is >= 820px
+820: {
+  slidesPerView: 3,
+  spaceBetween: 30
+},
+// when window width is >= 1280px
+1280	: {
+  slidesPerView: 3,
+  spaceBetween: 50
+}
+},
+  autoplay: {
+      delay: 100000,
+      disableOnInteraction: false,
+
+  },
+speed: 700,
+loop: true,
+centeredSlides: true,
+allowTouchMove: false,
+
+    on: {
+      init: function () {
 
       },
-  speed: 700,
-  loop: true,
-  centeredSlides: true,
+    },
+  });
+});
 
-        on: {
-          init: function () {
-
-          },
-        },
-      });
-    });
-*/
 //Reference Slider
 $(document).ready(function () {
 var referenceslider_wrapper = new Swiper(".referenceslider_wrapper", {
