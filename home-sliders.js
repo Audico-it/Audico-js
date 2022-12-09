1a0ed5c158643c6ef6fc04b8966824f90864b164
Audico-it <112904317+Audico-it@users.noreply.github.com>
2022-12-09 07:17:09 +0200

    Update home-sliders.js

--- home-sliders.js
+++ home-sliders.js
@@ -44,58 +44,6 @@ $(document).ready(function () {
 
 // Testimonial Slider
 $(document).ready(function () {
-  var testimonialslider_wrapper = new Swiper(".testimonialslider_wrapper", {
-      wrapperClass: "testimonialslider_list",
-  slideClass: "testimonialslider_item",
-  navigation: {
-    nextEl: '.testimonialslider-next',
-    prevEl: '.testimonialslider-previous'
-  },
-slidesPerView: 1,
-spaceBetween: 10,
-// Responsive breakpoints
-breakpoints: {
-      // when window width is >= 320px
-320: {
-  slidesPerView: 1,
-  spaceBetween: 20,
-},
-  // when window width is >= 560px
-560: {
-  slidesPerView: 3,
-  spaceBetween: 20
-},
-// when window width is >= 820px
-820: {
-  slidesPerView: 3,
-  spaceBetween: 30
-},
-// when window width is >= 1280px
-1280	: {
-  slidesPerView: 3,
-  spaceBetween: 50
-}
-},
-  autoplay: {
-      delay: 100000,
-      disableOnInteraction: false,
-
-  },
-speed: 700,
-loop: true,
-centeredSlides: true,
-allowTouchMove: false,
-
-    on: {
-      init: function () {
-
-      },
-    },
-  });
-});
-
-//Reference Slider
-$(document).ready(function () {
 var referenceslider_wrapper = new Swiper(".referenceslider_wrapper", {
 wrapperClass: "referenceslider_list",
 slideClass: "referenceslider_item",
@@ -109,11 +57,19 @@ el: '.referenceslider_pagination',
 clickable: true,
 
 },
-effect: 'coverflow',
-coverflowEffect: {
-  rotate: 30,
-  slideShadows: true,
-},
+ autoplay: {
+   delay: 5000,
+   disableOnInteraction: false,
+ },
+ effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 30,
+    slideShadows: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 speed: 1500,
 slidesPerView: 'auto',
 loop: true,
